95b94067657f70f6ffd16d7c07734f732cb1ccb7
Ryan Clark <ryanclark2k@gmail.com>
2019-09-28 17:20:27 -0700

    Fix draw rate problem

--- bayes.py
+++ bayes.py
@@ -1,12 +1,15 @@
 from math import factorial as fact
 
-total = 100
-n = 14
-w = 3
-l = 1
+total = 100   # Total games in series.
+n = 14        # Total games so far.
+w = 3         # Total wins so far.
+l = 1         # Total losses so far.
+draws = True  # Are there draws in this tournament?
 
-wr = (w + 1) / (n + 2)
-lr = (l + 1) / (n + 2)
+
+correction = 3 if draws else 2
+wr = (w + 1) / (n + correction)
+lr = (l + 1) / (n + correction)
 dr = 1 - wr - lr
 wp = 0
 
